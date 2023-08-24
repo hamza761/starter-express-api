fa9c7a9a7a8b0102d1231af5a8f6ff08cbfd11e1
hamza761 <hamzazahid761@gmail.com>
2023-08-24 18:50:39 +0500

    algo added

--- index.js
+++ index.js
@@ -2,24 +2,51 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 app.use(cors())
-// Dictionary to map IMEI numbers to device models (dummy data)
-const imeiToDeviceModel = {
-  '123456789': 'iPhone 11',
-  '1234': 'Samsung Galaxy S10',
-  // Add more entries here...
-};
+function isValidIMEI(imei) {
+    // Remove any non-numeric characters
+    imei = imei.replace(/[^\d]/g, '');
+  
+    // Check if the IMEI is 15 digits long and contains only numeric characters
+    if (/^\d{15}$/.test(imei) === false) {
+      return false;
+    }
+  
+    // Convert the IMEI string to an array of digits
+    const imeiArray = imei.split('').map(Number);
+  
+    // Implement the Luhn algorithm to validate the IMEI
+    let sum = 0;
+    for (let i = 0; i < 14; i++) {
+      let digit = imeiArray[i];
+      if (i % 2 === 1) {
+        digit *= 2;
+        if (digit > 9) {
+          digit -= 9;
+        }
+      }
+      sum += digit;
+    }
+  
+    // Calculate the check digit
+    const checkDigit = (10 - (sum % 10)) % 10;
+  
+    // Compare the calculated check digit with the last digit of the IMEI
+    return imeiArray[14] === checkDigit;
+  }
+  
+
+  app.get('/', (req, res) => {
+    res.send('ok')
+  })
 
 // Endpoint to get the device model from IMEI number
 app.get('/imei/:imeiNumber', (req, res) => {
-  const { imeiNumber } = req.params;
-  const model = imeiToDeviceModel[imeiNumber];
-setTimeout(() => {  
-  if (model) {
-    res.json({ model });
-  } else {
-    res.json({ model: 'Model not found for the provided IMEI number.' });
-  }
-}, 2000);
-});
+    const { imeiNumber } = req.params;
+    if (isValidIMEI(imeiNumber)) {
+      res.json({status: 'Valid IMEI'});
+    } else {
+        res.json({status: 'Invalid IMEI'});
+    }
+  });
 
 app.listen(process.env.PORT || 3000)
