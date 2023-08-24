c0509dbcd4bdba512868021c84b4ecf9368117e0
hamza761 <hamzazahid761@gmail.com>
2023-08-24 18:32:09 +0500

    p

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
-const express = require('express')
-const app = express()
-app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('Yo!')
-})
+const express = require('express');
+const cors = require('cors');
+const app = express();
+app.use(cors())
+// Dictionary to map IMEI numbers to device models (dummy data)
+const imeiToDeviceModel = {
+  '123456789': 'iPhone 11',
+  '1234': 'Samsung Galaxy S10',
+  // Add more entries here...
+};
+
+// Endpoint to get the device model from IMEI number
+app.get('/imei/:imeiNumber', (req, res) => {
+  const { imeiNumber } = req.params;
+  const model = imeiToDeviceModel[imeiNumber];
+setTimeout(() => {  
+  if (model) {
+    res.json({ model });
+  } else {
+    res.json({ model: 'Model not found for the provided IMEI number.' });
+  }
+}, 2000);
+});
+
 app.listen(process.env.PORT || 3000)
